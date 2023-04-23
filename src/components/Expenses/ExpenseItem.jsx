34af639b6c12013fcb146cfe8cf3e9eb7e15b0ff
Liat Katz <liatskatz@gmail.com>
2023-04-23 11:57:44 -0400

    Made the button ask user for prompt rather than just update with hardcoded text

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+// useState is used to define values as state where changes to the values should be reflected in the component function being called again
 import './ExpenseItem.css';
 import Card from '../UI/Card.jsx';
 import ExpenseDate from './ExpenseDate';
@@ -5,15 +7,20 @@ import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
     //the above "props" arg is one object that holds all the properties that we pass in the App.jsx file
+    const [title, setTitle]=useState(props.title); //requires an initial value to be passed into the useState function
+    //useState returns an array where the first value is the initial value and the second element is the updating function
+    
     const clickHandler=() => {
-        console.log("Clicked!!!!");
+        setTitle(prompt("What is the new expense?"));
+        // the above function calls the below function once again with the new value
+        console.log(title);
     }
 
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
     <div className="expense-item__description">
-        <h2>{props.title}</h2> 
+        <h2>{title}</h2> 
         {/* curly braces let us either do math expressions in them or use functions, or refer to variables created beforehand */}
         <div className="expense-item__price">${props.amount}</div>
         </div>
